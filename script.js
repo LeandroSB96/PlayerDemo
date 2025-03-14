3e97cb4710cee6b4681ae4a9e3e3b4141bc06159
Leandro Saint Bonnet <leandrosaint93@gmail.com>
2025-03-14 03:29:06 -0300

    Nueva actualización

--- script.js
+++ script.js
@@ -8,16 +8,4 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    // Código para el carrusel
-    const slides = document.querySelectorAll('.carousel-slide img');
-    let currentIndex = 0;
-
-    function showNextSlide() {
-        slides[currentIndex].classList.remove('active');
-        currentIndex = (currentIndex + 1) % slides.length;
-        slides[currentIndex].classList.add('active');
-    }
-
-    setInterval(showNextSlide, 3000);
-});
-
+    
