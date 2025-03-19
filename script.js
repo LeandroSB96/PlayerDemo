3f731df9a6f9da7923845885eb9824add278bcd4
Leandro Saint Bonnet <leandrosaint93@gmail.com>
2025-03-19 03:58:52 -0300

    Nueva actualización al front

--- script.js
+++ script.js
@@ -2,10 +2,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     const sidebarItems = document.querySelectorAll('.sidebar-item');
 
-    sidebarItems.forEach(item => {
-        item.addEventListener('click', function() {
-            this.classList.toggle('active');
-        });
-    });
+});
 
-    
