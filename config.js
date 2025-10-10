8b43a4d3c319ce569d52bd5162787fba20f3211f
Leandro Saint Bonnet <leandrosaint93@gmail.com>
2025-10-10 05:23:25 -0300

    Remove config.js from tracking and add to gitignore

--- config.js
+++
@@ -1,8 +0,0 @@
-// Configuracion Spotify API 
-const spotifyConfig = {
-    clientId: '8472edfd03c3461a9516729e6a2dac22',
-    clientSecret: 'fbf82229d04f4dd09857ee2911d54f3a',
-    redirectUri: 'http://127.0.0.1:5500/'
-};
-
-export default spotifyConfig;
