432afcbb5e644c3ec9fd79f3ff3d50e2d769a212
Leandro Saint Bonnet <leandrosaint93@gmail.com>
2025-10-10 03:36:28 -0300

    "ultima modificacion"

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Configuracion Spotify API 
 const spotifyConfig = {
     clientId: '8472edfd03c3461a9516729e6a2dac22',
-    clientSecret: '96f150ae3f7d4fc79489ad5faaecb794',
+    clientSecret: 'fbf82229d04f4dd09857ee2911d54f3a',
     redirectUri: 'http://127.0.0.1:5500/'
 };
 
